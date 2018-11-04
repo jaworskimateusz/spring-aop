bce49ca4f8622587a5c8962b54b254613ff365a2
jaworskimateusz <jaworskimateuszm@gmail.com>
2018-11-04 11:22:43 +0100

    Initial commit

---
+++ src/spring/aop/config/Config.java
@@ -0,0 +1,12 @@
+package spring.aop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("spring.aop")
+public class Config {
+
+}
